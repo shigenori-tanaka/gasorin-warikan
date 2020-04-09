e210bc28186db4f36d297b64c68a83fcc4b68107
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-04-09 16:19:55 +0900

    top.phpの読み込み

--- src/Calculation.php
+++ src/Calculation.php
@@ -1,11 +1,9 @@
 <?php
- 
-namespace Calculation;
- 
-class Calculation {
-    public function Add($mileage, $gasoline_mileage, $gasoline, $highway, $parking, $rent_a_car, $man) {
-        return  $total = ($mileage/$gasoline_mileage*$gasoline+$highway+$parking+$rent_a_car)/$man;
+    namespace Calculation;
+
+    class Calculation {
+        public function Add($mileage, $gasoline_mileage, $gasoline, $highway, $parking, $rent_a_car, $man) {
+            return  $total = ($mileage/$gasoline_mileage*$gasoline+$highway+$parking+$rent_a_car)/$man;
+        }
     }
- 
-}
 ?>
