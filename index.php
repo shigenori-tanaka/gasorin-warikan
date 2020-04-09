8bf85d2c90c496b520aad4988fcb091867a3d06d
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-04-09 19:19:04 +0900

    コード整形

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
         <div class="container">
             <p class="overview">車を使ったときのガソリン代や交通費の割り勘計算ができるアプリケーションです。<br><a href="use.html">使い方はこちらから</a></p>
         </div>
-
         <div class="container">
             <form action="top.php" method="GET">
                 <div class="row">
@@ -32,9 +31,9 @@
                     <div class="col-sm-5">
                         <h6>車の1リットルあたりの燃費（Km）</h6>
                         <p><a href="https://e-nenpi.com/enenpi/" target="_brank">実燃費一覧はこちら（外部サイト）</a></p>
-
                     </div>
-                    <div class="col-sm-5"><input type="text" name="gasoline_mileage" required>
+                    <div class="col-sm-5">
+                        <input type="text" name="gasoline_mileage" required>
                         <p style="display:inline">km</p>
                     </div>
                 </div>
@@ -48,33 +47,45 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-5"><h6>割り勘する人数</h6></div>
-                    <div class="col-sm-5"><input type="text" name="man" required><p style="display:inline">人</p></div>
+                    <div class="col-sm-5">
+                        <h6>割り勘する人数</h6>
+                    </div>
+                    <div class="col-sm-5">
+                        <input type="text" name="man" required>
+                        <p style="display:inline">人</p></div>
                 </div>
                 <div class="row">
                     <div class="col-sm-5">
                         <h4 id="option">オプション</h4>
                     </div>
                 </div>
-
-
-
                 <div class="row">
                     <div class="col-sm-5">
                         <h6>高速道路代金（円）</h6>
                         <p><a href="https://www.driveplaza.com/dp/SearchTop" target="_brank">高速道路代金検索はこちら（外部サイト）</a></p>
                     </div>
-                    <div class="col-sm-5"><input type="text" name="highway" style="text-align: right;" value="0">
+                    <div class="col-sm-5">
+                        <input type="text" name="highway" style="text-align: right;" value="0">
                         <p style="display:inline">円</p>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-5"><h6>駐車場代金（円</h6></div>
-                    <div class="col-sm-5"><input type="text" name="parking" style="text-align: right;" value="0"><p style="display:inline">円</p></div>
+                    <div class="col-sm-5">
+                        <h6>駐車場代金（円</h6>
+                    </div>
+                    <div class="col-sm-5">
+                        <input type="text" name="parking" style="text-align: right;" value="0">
+                        <p style="display:inline">円</p>
+                    </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-5"><h6>レンタカー代金（円）</h6></div>
-                    <div class="col-sm-5"><input type="text" name="rent_a_car" style="text-align: right;" value="0"><p style="display:inline">円</p></div>
+                    <div class="col-sm-5">
+                        <h6>レンタカー代金（円）</h6>
+                    </div>
+                    <div class="col-sm-5">
+                        <input type="text" name="rent_a_car" style="text-align: right;" value="0">
+                        <p style="display:inline">円</p>
+                    </div>
                 </div>
                 <div class="row">
                     <input class="btn btn-dark" role="button" type="submit" value="計算する" style="margin-right:10px">
@@ -89,12 +100,7 @@
                 </script>
                 <!-- クリアアラート設定 ここまで-->
             </form>
-
         </div>
-
-        
-
-
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
