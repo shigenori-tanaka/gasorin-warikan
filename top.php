8a1059dd2e3d62534797ba683327bdfba5a62afc
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-04-12 02:57:16 +0900

    viewport追加

--- top.php
+++ top.php
@@ -4,6 +4,7 @@
 <html lang="ja">
     <head>
         <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>enjoy Drive!!</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="stylesheet.css">
